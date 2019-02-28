fe5222cc7b7bbce956931d371f822b9032e0a384
Dylan Cox <dylanwjc@gmail.com>
2019-02-27 22:57:49 -0500

    roads instantiate on play

---
+++ InfiniteRunner_DylanCox/Assets/Scripts/RoadManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadManager : MonoBehaviour {
+	GameObject[] LoadedPieces;
+	// Use this for initialization
+	void Start () {
+		LoadedPieces = Resources.LoadAll<GameObject>("RoadPieces");
+
+		for (int i = 0; i < LoadedPieces.Length; i++) {
+
+			Instantiate (LoadedPieces [i]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
