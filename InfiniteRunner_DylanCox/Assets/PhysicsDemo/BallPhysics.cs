de9714e830cef6a1b9d38df0456f3371ab974357
Dylan Cox <dylanwjc@gmail.com>
2019-02-27 22:09:31 -0500

    first commit w running animation

---
+++ InfiniteRunner_DylanCox/Assets/PhysicsDemo/BallPhysics.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallPhysics : MonoBehaviour {
+
+	public float speed = 20; 
+	Rigidbody rb; 
+	Transform tCam;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		tCam = GameObject.Find ("Main Camera").transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float boost = 1f; 
+		Vector3 forward = tCam.forward;
+		Vector3 right = tCam.right;
+
+		if (Input.GetKey (KeyCode.Space)) {
+			boost = 2f;
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			rb.AddForce (forward * speed * boost);
+		} 
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			rb.AddForce (-forward * speed * boost);
+		} 
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			rb.AddForce (-right * speed * boost);
+		} 
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rb.AddForce (right * speed * boost);
+		} 
+	}
+}
---
+++ InfiniteRunner_DylanCox/Assets/PhysicsDemo/FollowCam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+	
+	public float zDistance;
+	public float yDistance;
+	public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.Find ("Sphere");
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate (Vector3.up, Time.deltaTime * 180);
+		} 
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (Vector3.up, Time.deltaTime * -180);
+		}
+		transform.position = Player.transform.position - transform.forward * zDistance;
+		transform.Translate (Vector3.up * yDistance);
+	}
+}
