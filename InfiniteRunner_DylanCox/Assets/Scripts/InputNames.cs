02b356f2fe2f4372712b7a73135ae08db38c6757
Dylan Cox <dylanwjc@gmail.com>
2019-03-04 12:36:58 -0500

    input logs for left right jump slide

---
+++ InfiniteRunner_DylanCox/Assets/Scripts/InputNames.cs
@@ -0,0 +1,9 @@
+﻿public class InputNames {
+
+	public const string horzontalAxis = "Horizontal"; 
+	public const string verticalAxis = "Vertical"; 
+	public const string jumpButton = "Jump"; 
+	public const string slideButton = "Slide"; 
+
+
+}
---
+++ InfiniteRunner_DylanCox/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	float h = 0f; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float hNew = Input.GetAxisRaw (InputNames.horzontalAxis); // returns -1, 0, or 1 with no smoothing
+		
+		if (!Mathf.Approximately (h, hNew) && !Mathf.Approximately(hNew, 0f)) {
+			//Debug.Log ("Horizontal axis:  " + Input.GetAxis (InputNames.horzontalAxis)); 
+			if (hNew < 0f) {
+				Debug.Log ("Left Input");
+			} else if (hNew > 0f) {
+				Debug.Log ("Right Input");
+			}
+
+			h = hNew;
+
+
+
+		}
+
+		// Jumping 
+		if (Input.GetButtonDown (InputNames.jumpButton)) {
+			Debug.Log ("Jump Button Pressed");
+		}
+
+		// Sliding 
+		if ( Input.GetButtonDown(InputNames.slideButton)) {
+			Debug.Log("Slide Button Pressed)");
+		} 
+
+	}
+
+}
