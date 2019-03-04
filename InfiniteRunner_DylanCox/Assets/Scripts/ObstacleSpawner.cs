07b6639f4eef7abb346bd2fe045c38289a2c5d07
Dylan Cox <dylanwjc@gmail.com>
2019-03-04 17:49:23 -0500

    loaded road obstacles

---
+++ InfiniteRunner_DylanCox/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour {
+	GameObject [] LoadedObstacles;
+
+	// Use this for initialization
+	void Start () {
+
+		//load obstacle prefabs
+		LoadedObstacles = Resources.LoadAll<GameObject>("Obstacles");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
