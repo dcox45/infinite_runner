17ffc37d068476e509feb446081de612c83f74a5
Dylan Cox <dylanwjc@gmail.com>
2019-03-07 11:39:55 -0500

    created enums.cs + CollisionManager.cs

---
+++ InfiniteRunner_DylanCox/Assets/CollisionManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour {
+
+	int playerMask;
+
+	// Use this for initialization
+	void Start () {
+
+		// Get layer mask for obstacle collision
+		playerMask = GetLayerMask((int)Layer.Obstacle);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	int GetLayerMask(params int[]indices) {
+
+		int mask = 0; 
+
+		for (int i = 0; i < indices.Length; i++) {
+			mask |= 1 << indices [i];
+		}
+		return mask;
+	}
+
+	int GetLayerIgnoreMask(params int[] indices) {
+		return ~GetLayerIgnoreMask (indices);
+	}
+
+	// HELPERS 
+	void AddLayers(ref int mask, params int[] indices) {
+		mask |= GetLayerMask (indices);
+	}
+
+	void RemoveLayers(ref int mask, params int[] indices) {
+		mask &= ~GetLayerMask (indices);
+	}
+}
---
+++ InfiniteRunner_DylanCox/Assets/Scripts/Enums.cs
@@ -0,0 +1,13 @@
+﻿	public enum Layer {
+		//Built-in Layers 
+		Default = 0, 
+		TransparentFX,
+		IgnoreRayCast,
+		Water = 4, 
+		UI,
+
+		//User Layers
+		Player = 8,
+		Obstacle,
+	}
+}
